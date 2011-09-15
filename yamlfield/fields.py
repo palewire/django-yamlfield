7de08c45f6cb7092033e40a50b4631ffa8b8808d
palewire <ben.welsh@gmail.com>
2011-09-15 10:32:36 -0700

    My preexisting code

---
+++ yamlfield/fields.py
@@ -0,0 +1,51 @@
+import yaml
+from django.db import models
+from django.core.serializers.pyyaml import DjangoSafeDumper
+
+
+class YAMLField(models.TextField):
+    """
+    YAMLField is a TextField that serializes and deserializes YAML data
+    from the database.
+    
+    Based on https://github.com/bradjasper/django-jsonfield
+    """
+    # Used so to_python() is called
+    __metaclass__ = models.SubfieldBase
+    
+    def to_python(self, value):
+        """
+        Convert our YAML string to a Python object after we load it from the DB.
+        """
+        if value == "":
+            return None
+        try:
+            if isinstance(value, basestring):
+                return yaml.load(value)
+        except ValueError:
+            pass
+        return value
+    
+    def get_db_prep_save(self, value):
+        """
+        Convert our Python object to a string of YAML before we save.
+        """
+        if not value or value == "":
+            return None
+        if isinstance(value, (dict, list)):
+            value = yaml.dump(value, Dumper=DjangoSafeDumper, 
+                default_flow_style=False)
+        return super(YAMLField, self).get_db_prep_save(value)
+    
+    def value_from_object(self, obj):
+        """
+        Returns the value of this field in the given model instance.
+        
+        We need to override this so that the YAML comes out properly formatted
+        in the admin widget.
+        """
+        value = getattr(obj, self.attname)
+        if not value or value == "":
+            value
+        return yaml.dump(value, Dumper=DjangoSafeDumper,
+            default_flow_style=False)
---
+++ yamlfield/tests.py
@@ -0,0 +1,48 @@
+import datetime
+from fields import YAMLField
+from django.db import models
+from django.test import TestCase
+
+class YAMLModel(models.Model):
+    yaml = YAMLField()
+
+class YAMLFieldTest(TestCase):
+    """YAMLField Wrapper Tests"""
+    
+    def test_yaml_field_create(self):
+        """Test saving a YAML object in our YAMLField"""
+        yaml_obj =  {
+            "item_1": "this is a yaml blah",
+            "blergh": "hey, hey, hey"
+        }
+        obj = YAMLModel.objects.create(yaml=yaml_obj)
+        new_obj = YAMLModel.objects.get(id=obj.id) 
+        self.failUnlessEqual(new_obj.yaml, yaml_obj)
+    
+    def test_yaml_field_modify(self):
+        """Test modifying a YAML object in our YAMLField"""
+        yaml_obj_1 = {'a': 1, 'b': 2}
+        yaml_obj_2 = {'a': 3, 'b': 4}
+        obj = YAMLModel.objects.create(yaml=yaml_obj_1)
+        self.failUnlessEqual(obj.yaml, yaml_obj_1)
+        obj.yaml = yaml_obj_2
+        self.failUnlessEqual(obj.yaml, yaml_obj_2)
+        obj.save()
+        self.failUnlessEqual(obj.yaml, yaml_obj_2)
+        self.assert_(obj)
+    
+    def test_yaml_field_load(self):
+        """Test loading a YAML object from the DB"""
+        yaml_obj_1 = {'a': 1, 'b': 2}
+        obj = YAMLModel.objects.create(yaml=yaml_obj_1)
+        new_obj = YAMLModel.objects.get(id=obj.id)
+        self.failUnlessEqual(new_obj.yaml, yaml_obj_1)
+    
+    def test_yaml_list(self):
+        """Test storing a yaml list"""
+        yaml_obj = ["my", "list", "of", 1, "objs", {"hello": "there"}]
+        obj = YAMLModel.objects.create(yaml=yaml_obj)
+        new_obj = YAMLModel.objects.get(id=obj.id)
+        self.failUnlessEqual(new_obj.yaml, yaml_obj)
+
+
