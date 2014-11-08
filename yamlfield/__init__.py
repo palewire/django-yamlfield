3273ce8849b6b895513f6d5a3182fce0b16da48b
palewire <ben.welsh@gmail.com>
2014-11-08 13:47:27 -0800

    Refactored tests to work with Django 1.7

---
+++ yamlfield/__init__.py
@@ -0,0 +1,2 @@
+from __future__ import absolute_import
+default_app_config = 'yamlfield.apps.YAMLFieldAppConfig'
---
+++ yamlfield/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YAMLFieldAppConfig(AppConfig):
+    name = 'yamlfield'
+    verbose_name = "YAML field"
