3c732fe1da056bea84a0f60d297bd7b7d2a04689
Simon Meers <simon@simonmeers.com>
2015-02-23 11:18:58 +1100

    Support ordered dictionary data.

--- yamlfield/fields.py
+++ yamlfield/fields.py
@@ -1,7 +1,43 @@
+from collections import OrderedDict
 import six
 import yaml
 from django.db import models
 from django.core.serializers.pyyaml import DjangoSafeDumper
+
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    """
+    From http://stackoverflow.com/a/21912744/284164
+    # usage example:
+    ordered_load(stream, yaml.SafeLoader)
+    """
+    class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+
+def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+    """
+    From http://stackoverflow.com/a/21912744/284164
+    # usage example:
+    ordered_dump(data, Dumper=yaml.SafeDumper)
+    """
+    class OrderedDumper(Dumper):
+        pass
+
+    def _dict_representer(dumper, data):
+        return dumper.represent_mapping(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+            data.items())
+    OrderedDumper.add_representer(OrderedDict, _dict_representer)
+    return yaml.dump(data, stream, OrderedDumper, **kwds)
 
 
 class YAMLField(six.with_metaclass(models.SubfieldBase, models.TextField)):
@@ -20,7 +56,7 @@ class YAMLField(six.with_metaclass(models.SubfieldBase, models.TextField)):
             return None
         try:
             if isinstance(value, six.string_types):
-                return yaml.load(value)
+                return ordered_load(value, yaml.SafeLoader)
         except ValueError:
             pass
         return value
@@ -32,11 +68,12 @@ class YAMLField(six.with_metaclass(models.SubfieldBase, models.TextField)):
         if not value or value == "":
             return ""
         if isinstance(value, (dict, list)):
-            value = yaml.dump(
+            value = ordered_dump(
                 value,
                 Dumper=DjangoSafeDumper,
                 default_flow_style=False
             )
+
         return super(YAMLField, self).get_db_prep_save(
             value,
             connection=connection
@@ -52,7 +89,7 @@ class YAMLField(six.with_metaclass(models.SubfieldBase, models.TextField)):
         value = getattr(obj, self.attname)
         if not value or value == "":
             return value
-        return yaml.dump(
+        return ordered_dump(
             value,
             Dumper=DjangoSafeDumper,
             default_flow_style=False
